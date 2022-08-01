49f54c6bc0901592dc7c38fa4c855b6b3febcefa
Nick <Nick_II2000@mail.ru>
2022-08-01 20:27:09 +0600

    6-th commit

--- js/script.js
+++ js/script.js
@@ -1,51 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    // Modal
-
-    const modalTrigger = document.querySelectorAll('[data-modal]'),
-        modal = document.querySelector('.modal'),
-        prevModalDialog = document.querySelector('.modal__dialog');
-
-    function showModalByScroll() {
-        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
-            openModal();
-        }
-    }
-
-    window.addEventListener('scroll', showModalByScroll);
-
-    function openModal() {
-        modal.classList.add('show');
-        modal.classList.remove('hide');
-        // modal.classList.toggle('show');
-        document.body.style.overflow = 'hidden';
-        clearInterval(modalTimerId);
-        window.removeEventListener('scroll', showModalByScroll);
-    }
-
-    modalTrigger.forEach(btn => btn.addEventListener('click', openModal));
-
-    function closeModal() {
-        modal.classList.add('hide');
-        modal.classList.remove('show');
-        // modal.classList.toggle('show');
-        document.body.style.overflow = '';
-    }
-
-    modal.addEventListener('click', (event) => {
-        if (event.target === modal || event.target.getAttribute('data-close') == '') {
-            closeModal();
-        }
-    });
-
-    document.addEventListener('keydown', (event) => {
-        if (event.code === 'Escape' && modal.classList.contains('show')) {
-            closeModal();
-        }
-    });
-
-    const modalTimerId = setTimeout(openModal, 50000);
-
     // Forms
 
     const forms = document.querySelectorAll('form');
