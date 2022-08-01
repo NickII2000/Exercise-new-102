71bac83d5c457ed9f9262fccfe991a8e525df98b
Nick <Nick_II2000@mail.ru>
2022-08-01 21:01:51 +0600

    10-th commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
-
-
-
+    const tabs = require('./modules/tabs'),
+        modal = require('./modules/modal'),
+        timer = require('./modules/timer'),
+        cards = require('./modules/cards'),
+        calc = require('./modules/calc'),
+        forms = require('./modules/forms'),
+        slider = require('./modules/slider');
 });
